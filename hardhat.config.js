1f88b2f561fa237462c05a5ede2fc7303078d034
pspbps <foriscompany1916@gmail.com>
2026-01-28 14:47:49 +0800

    Add Hardhat config for PSP reference contracts

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
